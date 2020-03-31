ffa33e329177d07cd1c2ec8e60d883e26a8add19
Kevin Arditya <Kevin.Arditya@btpn.com>
2020-04-01 01:39:29 +0700

    fix main blog page

--- eveneweb/resources/views/blog.blade.php
+++ eveneweb/resources/views/blog.blade.php
@@ -4,35 +4,35 @@
 
     <!-- LIGHT SECTION -->
     <section class="lightSection clearfix pageHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="page-title">
-                <h2>Blog</h2>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <ol class="breadcrumb float-right">
-                <li>
-                  <a href="index.html">Home</a>
-                </li>
-                <li class="active">Blog </li>
-              </ol>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="page-title">
+              <h2>Blog</h2>
             </div>
           </div>
+          <div class="col-md-6">
+            <ol class="breadcrumb float-right">
+              <li>
+                <a href="{{ url('/') }}">Home</a>
+              </li>
+              <li class="active">Blog </li>
+            </ol>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <!-- MAIN CONTENT SECTION -->
-      <section class="mainContent clearfix blogPage">
-        <div class="container">
-          <div class="row">
-            @foreach($list as $lists)
+    <!-- MAIN CONTENT SECTION -->
+    <section class="mainContent clearfix blogPage">
+      <div class="container">
+        <div class="row">
+          @foreach($list as $lists)
             <div class="col-md-12">
               <div class="thumbnail">
-                <center>
-                <a href="{{ url('blog/1') }}"><img class="mw-100" src="img/blog/ultah.jpg" alt="blog-image"></a>
-                </center>
+                <div style="text-align:center">
+                  <a href="{{ url('blog/'.$lists->id) }}"><img class="mw-100" src="img/blog/{{$lists->thumbnail}}" alt="blog-image"></a>
+                </div>
                 <div class="caption">
                   <div class="row">
                     <div class="col-md-2 order-md-12">
@@ -50,19 +50,20 @@
                 </div>
               </div>
             </div>
-            @endforeach
+          @endforeach
           <div class="row">
             <div class="col-md-12">
               <div class="text-center">
                 <ul class="pagination justify-content-center">
 
                 {{ $list->links() }}
-              
+
                 </ul>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
 @endsection
